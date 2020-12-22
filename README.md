44c8408faabf6ce9bae845bc8e838eb31da01f71
Mingyang Li <tslmy@outlook.com>
2020-12-21 21:18:29 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# 成语接龙 IdiomGraph-gv
+
+(This project doesn't make sense in English.)
+
+成语接龙表。使用`graphviz`实现。
+
+## Usage
+
+1. Run this command first to retrieve a list of idioms:
+
+```shell
+curl https://raw.githubusercontent.com/fighting41love/funNLP/master/data/%E6%88%90%E8%AF%AD%E8%AF%8D%E5%BA%93/ChengYu_Corpus%EF%BC%885W%EF%BC%89.txt > idioms.txt
+```
+
+2. Run the main script:
+
+```shell
+python main.py
+```
+
+## References
+
+类似的项目有[IdiomGraph](https://github.com/shawlu95/IdiomGraph)——使用neo4j实现的。
---
+++ main.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+from graphviz import Digraph
+from tqdm import tqdm
+
+N = 2000
+
+
+def mergeIdiomsByRule(rule, dictionary):
+    '''
+    For each idiom list in the given dictionary, merge all idioms failing the
+    rule into one.
+    '''
+    with tqdm(total=len(dictionary), desc='Merging idioms by rule') as pbar:
+        for key, this_idioms in dictionary.items():
+            pbar.update()
+            _ = map(rule, this_idioms)  # TODO: Is this needed?
+            # Collect idioms based on test result.
+            failed_idioms = []
+            passed_idioms = []
+            for idiom in this_idioms:
+                (passed_idioms if rule(idiom) else failed_idioms).append(idiom)
+            # Update the entry to be the list connecting idioms extended with
+            # a combined idiom.
+            if len(failed_idioms) > 0:
+                dictionary[key] = passed_idioms
+                dictionary[key].append('\n'.join(failed_idioms))
+
+
+if __name__ == "__main__":
+    with open('idioms.txt', 'r') as file:
+        # Load lines and remove new lines.
+        idioms = map(lambda x: x.strip(), file)
+
+    # Filter for 4-letter idioms only.
+    idioms = filter(lambda x: len(x) == 4, idioms)
+    # Make sure that there's not repetitions.
+    idioms = set(idioms)
+    # Only take the first N idioms.
+    idioms = list(idioms)[:N]
+
+    beginnings = defaultdict(list)
+    endings = defaultdict(list)
+    for idiom in idioms:
+        beginnings[idiom[0]].append(idiom)
+        endings[idiom[-1]].append(idiom)
+
+    mergeIdiomsByRule(
+        rule=lambda idiom: idiom[0] in endings, dictionary=endings)
+    mergeIdiomsByRule(
+        rule=lambda idiom: idiom[-1] in beginnings, dictionary=beginnings)
+
+    num_iterations = sum(len(starting_idioms) * len(endings[key])
+                         for key, starting_idioms in beginnings.items())
+    g = Digraph(name='Idioms', node_attr={'shape': 'plaintext'}, strict=True)
+    with tqdm(total=num_iterations, desc='Generating graph') as pbar:
+        for key, starting_idioms in beginnings.items():
+            for starting_idiom in starting_idioms:
+                for ending_idiom in endings[key]:
+                    pbar.update()
+                    g.node(key, shape='circle')
+                    g.edge(ending_idiom, key)
+                    g.edge(key, starting_idiom)
+
+    g.render()
